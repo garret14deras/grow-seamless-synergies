87d9b5aeff2f938db94337b15faf7c7edc86c017
garret14deras <139816612+garret14deras@users.noreply.github.com>
2023-11-30 08:54:51 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+public class RandomDataGenerator {
+
+}
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+
+    public static void main(String[] args) {
+        }
+            System.out.println("Random Number: " + item);
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+        }
